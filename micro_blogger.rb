7df64fcc733efb059132f6b31ff18def654b78bf
ajrussellaudio <ajrussellaudio@gmail.com>
2014-06-09 19:28:28 +0100

    Iteration 2 done

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -7,4 +7,34 @@ class MicroBlogger
 		puts "Initializing"
 		@client = JumpstartAuth.twitter
 	end
+
+	def tweet(message)
+		if message.length <= 140 
+			@client.update(message) 
+		else 
+			puts "Status is over 140 characters."
+		end
+	end
+
+	def run
+		system "clear"
+		puts "Welcome to the JSL Twitter Client!"
+		command = ""
+		while command != "q"
+			printf "enter command: "
+			input = gets.chomp
+			parts = input.split(" ")
+			command = parts[0]
+			case command
+			when 'q' then puts "Goodbye!"
+			when 't' then tweet(parts[1..-1].join(" "))
+			else
+				puts "Sorry, I don't know how to #{command}"
+			end
+		end
+	end
+
 end
+
+blogger = MicroBlogger.new
+blogger.run
